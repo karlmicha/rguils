a7873775d244473810f22347281697aaf4dbaefd
karlmicha <karlmicha@df55e112-d232-ff18-fce3-c1d5acedaa7b>
2010-10-15 19:57:50 +0000

    added license

--- resources/userlib.py
+++ resources/userlib.py
@@ -1,9 +1,23 @@
-"""userlib.py
-   Adds a user-defined directory and all *.sikuli subdirectories to the python
-   module path, so that user-defined modules and Sikuli projects can be
-   imported into python scripts run with Sikuli Script.
-   Copy this file into the Sikuli installation directory, e.g. on Windows:
-   C:\Program Files\Sikuli\userlib.py
+"""
+Copyright (c) 2010 Karl-Michael Schneider
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 """
 
 import sys, os, os.path
